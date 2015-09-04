f3d24f222663a3b9d714e182af9b71c4194a8b24
Jeff Jones <jeff.jones1@gmail.com>
2015-09-04 17:11:34 -0400

    refactored hacky solution

--- js/views/app.js
+++ js/views/app.js
@@ -8,18 +8,13 @@ App.Views.App = Backbone.View.extend({
   },
   captureInput: function(){
     event.preventDefault();
-    state = $('#stateSearch').val();
-    city = $('#citySearch').val();
-    App.Collections.forecasts = new App.Collections.Forecasts();
-    App.Collections.forecasts.url = "http://api.wunderground.com/api/c3a3c31a88cc16fa/forecast10day/forecastday/q/" + state +'/'+city+".json";
-    console.log(App.Collections.forecasts.url);
+    this.state = $('#stateSearch').val();
+    this.city = $('#citySearch').val();
     this.getWeather();
   },
   getWeather: function(){
-    // App.Collections.forecasts = new App.Collections.Forecasts();
-    // App.Views.forecastList = new App.Views.ForecastList({collection: App.Collections.forecasts});
-    // console.log(App.Views.forecastList.url);
-    console.log(App.Collections.forecasts)
+    App.Collections.forecasts = new App.Collections.Forecasts();
+    App.Collections.forecasts.url = "http://api.wunderground.com/api/c3a3c31a88cc16fa/forecast10day/forecastday/q/" + this.state +'/'+this.city+".json"
     App.Views.forecastList = new App.Views.ForecastList({collection: App.Collections.forecasts});
     App.Collections.forecasts.fetch({reset: true});
   }
